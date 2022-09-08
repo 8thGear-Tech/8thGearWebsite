844e3b075b80de39e8a86f234237ef1dbcc1878e
joshboy4 <joshboy395@gmail.com>
2022-09-08 10:31:56 +0100

    worked on mediagallerycards

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-
 import * as React from "react";
+import { Component } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home";
+import { InstagramBtn } from "./components/Buttons/BlogBtn";
+import { LinkedInBtn } from "./components/Buttons/BlogBtn";
+import { ShareBtn } from "./components/Buttons/BlogBtn";
+import { MediaCards } from "./components/AllCards/MediaCards";
+import { GalleryGizCards } from "./components/AllCards/MediaCards";
+import { galleryusadf } from "./components/AllCards/MediaCards";
 
 function App() {
   return (
-    <BrowserRouter> 
-    
+    <BrowserRouter>
       {/* <h1>Welcome to React Router!</h1> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-
+      <Routes>{/* <Route path="/" element={<InstagramBtn />} /> */}</Routes>
+      <InstagramBtn />
+      <LinkedInBtn />
+      <ShareBtn />
+      <MediaCards />
+      <GalleryGizCards />
+      <galleryusadf />
     </BrowserRouter>
-   
   );
 }
 
--- src/components/AllCards/MediaCards.js
+++ src/components/AllCards/MediaCards.js
@@ -1,4 +1,97 @@
 //GalleryCard
-//GalleryMediaCards
-//AllPortfoliosLogoCard
-//ProjectDeliveryManagedServicesCard
+
+import galleryData from "../../data/galleryS4.json";
+import gallerypictures from "../../data/galleryGIZ.json";
+import galleryusadf from "../../data/galleryusadf.json";
+
+export const SingleMediaCards = (props) => {
+  const { image, id } = props;
+  return (
+    <>
+      <img className="" style={{ width: 358.8 }} src={image} alt={id} />
+    </>
+  );
+};
+
+export const MediaCards = () => {
+  return (
+    <>
+      <div className="row d-flex justify-content-between col-md-4 ">
+        {galleryData.gallerypictures.map((items) => {
+          return (
+            <>
+              <div className="col" key={items.id}>
+                <SingleMediaCards {...items} />
+              </div>
+              ;
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export const SingleGalleryGizCards = (props) => {
+  const { image, id } = props;
+  return (
+    <>
+      <img className="" style={{ width: 358.8 }} src={image} alt={id} />
+    </>
+  );
+};
+
+export const GalleryGizCards = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-4">
+          {gallerypictures.galleryGIZ.map((items) => {
+            return (
+              <>
+                <div className="" key={items.id}>
+                  <SingleGalleryGizCards {...items} />
+                </div>
+                ;
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+// export const SingleUsadfCards = (props) => {
+// //   const { image, id } = props;
+// //   return (
+// //     <>
+// //       <img className="" style={{ width: 358.8 }} src={image} alt={id} />
+// //     </>
+// //   );
+// // };
+
+// // export const UsadfCards = () => {
+// //   return (
+// //     <>
+// //       <div className="row">
+// //         <div className="col-md-4">
+// //           {galleryData.gallerypictures.map((items) => {
+// //             return (
+// //               <>
+// //                 <div className="d-flex justify-content-between" key={items.id}>
+// //                   <SingleUsadfCards {...items} />
+// //                 </div>
+// //                 ;
+// //               </>
+// //             );
+// //           })}
+// //         </div>
+// //       </div>
+// //     </>
+// //   );
+// // };
+
+// //GalleryMediaCards
+// //AllPortfoliosLogoCard
+// //ProjectDeliveryManagedServicesCard
