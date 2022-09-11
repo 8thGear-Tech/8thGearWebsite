3f8df9c4af0b989de05bcd8a3d913f0faaec9989
John-nathan-the-one <peterjasper2nd@gmail.com>
2022-09-11 15:12:57 +0100

    Push from Jonathans branch for AllPortfoliosLogoCard and ProjectDeliveryManagedServicesCard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 
 import * as React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/home";
+import Portfolios from "./pages/portfolios";
+import { BookNowBtn } from "./components/Buttons/ServicesBtn";
+import { BookaSessionBtn } from "./components/Buttons/ServicesBtn";
+import { BookaFreeSessionBtn } from "./components/Buttons/ServicesBtn";
+import { OnHoverReadMoreBtn } from "./components/Buttons/ServicesBtn";
+import { AllPortfoliosLogoCard } from "./components/AllCards/MediaCards";
+import { ProjectDeliveryManagedServicesCard } from "./components/AllCards/MediaCards";
 
 function App() {
   return (
@@ -10,6 +17,13 @@ function App() {
       {/* <h1>Welcome to React Router!</h1> */}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="portfolios" element={<Portfolios />} />
+        <Route path="button1" element={<BookNowBtn />} />
+        <Route path="button2" element={<BookaSessionBtn />} />
+        <Route path="button3" element={<BookaFreeSessionBtn />} />
+        <Route path="button4" element={<OnHoverReadMoreBtn />} />
+        <Route path="button5" element={<AllPortfoliosLogoCard />} />
+        <Route path="button6" element={<ProjectDeliveryManagedServicesCard />} />
       </Routes>
 
     </BrowserRouter>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,3 +7,12 @@
 //ProjectDeliveryPageHero
 //OurModelPageHero
 //PortfoliosandTeamPageHero
+
+
+const PortfoliosandTeamPageHero = () => {
+    return (<div className="bg-dark text-center text-white px-1 py-1 h1">
+        <h1>The Venture Studio model for entrepreneurship, combining comapny building with venture funding.The Venture Studio model for entrepreneurship, combining comapny building with venture funding.</h1>
+    </div>)
+}
+
+export default PortfoliosandTeamPageHero
