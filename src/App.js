2d48795736be6b8b01a249fd780aebd70d62db8f
ProfInvincible <muoghaluijeoma9@gmail.com>
2022-09-13 16:19:40 +0100

    message

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import * as React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home";
 import NavBar from "./components/Navbar";
-import { ManagedServicesPageTestimonialCard } from "../src/components/AllCards/TestimonialCards";
-import { Lad } from "./components/Forms";
-import { CoWorkingPageTestimonialCard } from "../src/components/AllCards/TestimonialCards";
-import { AboutUs } from "./components/AllCards/TestimonialCards";
+// import { ManagedServicesPageTestimonialCard } from "../src/components/AllCards/TestimonialCards";
+// import { Lad } from "./components/Forms";
+// import { CoWorkingPageTestimonialCard } from "../src/components/AllCards/TestimonialCards";
+// import { AboutUs } from "./components/AllCards/TestimonialCards";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
-      <ManagedServicesPageTestimonialCard />
-      <Lad />
-      <CoWorkingPageTestimonialCard />
-      <AboutUs />
+      {/* <ManagedServicesPageTestimonialCard /> */}
+      {/* <Lad /> */}
+      {/* <CoWorkingPageTestimonialCard /> */}
+      {/* <AboutUs /> */}
 
       <Routes>
         <Route path="/" element={<Home />} />
