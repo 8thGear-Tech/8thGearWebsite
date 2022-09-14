fd4bd2ece1ba7f5a796ab75f5d36eb5aaa4f6ad7
Iamsamizane <samuelakanbi2303@gmail.com>
2022-09-14 11:22:53 +0100

    update card

--- src/components/AllCards/TriggeredCards.js
+++ src/components/AllCards/TriggeredCards.js
@@ -6,6 +6,7 @@
 
 import Image  from "../../assets/images/TeamImage.jpg"
 import logo from "../../assets/images/logo.png"
+import link from "../../assets/images/LinkedIn_icon.png"
 
 export const EightgeariansPopUpCard = () => {
     return (
@@ -13,11 +14,11 @@ export const EightgeariansPopUpCard = () => {
     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
   View more
 </button>
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="d-flex justify-content-between mx-4 my-2">
-        <img src={logo} width={50} className="" id="staticBackdropLabel"/>
+<div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="d-flex justify-content-between mx-4 my-2">
+        <img src={logo} width={50} id="staticBackdropLabel"/>
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       
@@ -30,6 +31,7 @@ export const EightgeariansPopUpCard = () => {
                 cillum dolore eu fugiat nulla dolor in reprehenderit in voluptate 
                 velit essecillum dolore eu fugiat nulla reprehenderit in</p>
             </div>
+            <img src={link} className="mb-4" width={25}/>
         </div>
         </div>
         </div>
@@ -47,27 +49,27 @@ export const EightgeariansPopUpCard = () => {
 export const EIRDataPopUpCard = () => {
   return (
       <>
-  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
 View more
 </button>
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-<div class="modal-dialog">
-  <div class="modal-content">
-  <img src={Image} class="w-100 mb-4" alt="..."/>
-  <div class="card-img-overlay">
-    <div class="d-flex justify-content-between mx-1 my-1">
-      <img src={logo} width={40} class="modal-title" id="staticBackdropLabel"/>
-      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+<div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div className="modal-dialog">
+  <div className="modal-content">
+  <img src={Image} className="w-100 mb-4" alt="..."/>
+  <div className="card-img-overlay">
+    <div className="d-flex justify-content-between mx-1 my-1">
+      <img src={logo} width={40} className="modal-title" id="staticBackdropLabel"/>
+      <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
     </div>
     
   <div className="cards">
           
           <br></br>
-          <h4 class="">DAMILOLA OBIDAIRO</h4>
+          <h4 className="">DAMILOLA OBIDAIRO <img src={link} className="" width={25}/></h4>
           <h6 className="">CO- FOUNDER</h6>
-          <div class="card-body">
-              <p class="card-text mb-4">dolor in reprehenderit in voluptate velit esse, cillum dolore eu fugiat nulla dolor in reprehenderit in voluptate velit esse
+          <div className="card-body">
+              <p className="card-text mb-4">dolor in reprehenderit in voluptate velit esse, cillum dolore eu fugiat nulla dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla reprehenderit in voluptate velit esse.
 dolor in reprehenderit in voluptate velit esse, cillum dolore eu fugiat nulla dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla reprehenderit in voluptate velit esse.
--- src/components/Forms/ServicesForms.js
+++ src/components/Forms/ServicesForms.js
@@ -26,17 +26,17 @@ const Request = () => {
   Request for a proposal
 </button>
 
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog modal-sm">
-    <div class="modal-content">
-    <div class="modal-header m-2">
-        <img src={logo} width={40} className="" id="staticBackdropLabel"/>
+<div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div className="modal-dialog modal-sm">
+    <div className="modal-content">
+    <div className="modal-header m-2">
+        <img src={logo} width={40} id="staticBackdropLabel"/>
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
+      <div className="modal-body">
         <form onSubmit={handleSubmit}>
-<div className="background" >
-<h4 class=" request mb-4">Request Form</h4>
+
+<h4 className=" request mb-4">Request Form</h4>
 
 <div className="Vessel mb-3">
    <input onChange={(e) => setFormData({...formData, email: e.target.value})} value={formData.email} type="text" name="email" id="email" className="myInput form-control" placeholder="Email"></input>
@@ -49,19 +49,18 @@ const Request = () => {
 
 <div className=" Vessel mb-3">
     <label for="debrief" className="form-label"></label>
-    <textarea class="myInput form-control" id="debrief" rows="3" onChange={(e) => setFormData({...formData, debrief: e.target.value})} value={formData.debrief} type="text" name="debrief" placeholder="Debrief"></textarea>
+    <textarea className="myInput form-control" id="debrief" rows="3" onChange={(e) => setFormData({...formData, debrief: e.target.value})} value={formData.debrief} type="text" name="debrief" placeholder="Debrief"></textarea>
 </div>
-<div class="col-12 mb-3">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck"/>
-      <label class="form-check-label" for="gridCheck">
+<div className="col-12 mb-3">
+    <div className="form-check">
+      <input className="form-check-input" type="checkbox" id="gridCheck"/>
+      <label className="form-check-label" for="gridCheck">
       Click to join our  Newsletter
     </label>
     </div>
     </div>
     <div className="sub">
 <button type="button" className="submit">Submit</button>
-    </div>
     </div>
 </form>
 
