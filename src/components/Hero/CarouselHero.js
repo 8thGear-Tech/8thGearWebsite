6a9dc3813a36f996c9e304736deb5dae6bdf2cc5
ProfInvincible <muoghaluijeoma9@gmail.com>
2022-09-29 10:12:55 +0100

    navbarfaqcarousel

--- src/components/Hero/CarouselHero.js
+++ src/components/Hero/CarouselHero.js
@@ -41,11 +41,27 @@ export const EventPageHero = () => {
                 entrepreneurship, combining company building.
               </p>
             </div>
+            {/* for mobile view */}
+            <div className="carousel-caption d-lg-none d-md-none d-sm-block d-xs-block">
+              <p className="mt-4 text-dark">
+                The Venture Studio model for entrepreneurship, combining comapny
+                building with venture funding.The Venture Studio model for
+                entrepreneurship, combining company building.
+              </p>
+            </div>
           </div>
           <div className="carousel-item">
             <img src={homepage} className="d-block w-100" alt="..." />
             <div className="carousel-caption EventPageHero ">
               <p>
+                The Venture Studio model for entrepreneurship, combining comapny
+                building with venture funding.The Venture Studio model for
+                entrepreneurship, combining company building.
+              </p>
+            </div>
+            {/* for mobile view */}
+            <div className="carousel-caption d-lg-none d-md-none d-sm-block d-xs-block">
+              <p className="mt-4 text-dark">
                 The Venture Studio model for entrepreneurship, combining comapny
                 building with venture funding.The Venture Studio model for
                 entrepreneurship, combining company building.
