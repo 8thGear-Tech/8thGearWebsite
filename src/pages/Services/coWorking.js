38e9935d5c7cdd64813cdbf0c54a432121558da5
8thGear-Tech <opeyemibioku@gmail.com>
2022-10-17 15:50:38 +0100

    Merged with branch name opeyemi

--- src/pages/Services/coWorking.js
+++ src/pages/Services/coWorking.js
@@ -50,13 +50,13 @@ const CoWorking = () => {
               <i class="bi bi-file-fill" style={{ fontSize: "3rem" }}></i>
             </div>
             <div className="c3-element3">
-              <p> Fully Equipped Conference room</p>
+              <p> Fully Equipped Conference room.</p>
               <i className="bi bi-laptop" style={{ fontSize: "3rem" }}></i>
             </div>
           </div>
           <div className="grid2">
             <div className="c4-element1">
-              <p> High Speed and secured internet service</p>
+              <p> High Speed and secured internet service.</p>
               <i className="bi bi-wifi" style={{ fontSize: "3rem" }}></i>
             </div>
           </div>
