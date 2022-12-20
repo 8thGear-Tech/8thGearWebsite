8e1ea905a324c772d7b86694513bb8e470b02bfe
8thGear-Tech <opeyemibioku@gmail.com>
2022-12-20 10:18:22 +0100

    pulled master branch

--- src/components/Buttons/PortfolioBtn.js
+++ src/components/Buttons/PortfolioBtn.js
@@ -2,21 +2,21 @@
 //NextBtn
 
 export const PreviousBtn = () => {
-    return (
-        <>
-        <button type= "button" className= "PreviousBtn">
-            Prev
-            </button>
-        </>
-    )
-}
+  return (
+    <>
+      <button type="button" className="PreviousBtn">
+        Prev
+      </button>
+    </>
+  );
+};
 
 export const NextBtn = () => {
-    return (
-        <>
-        <button type= "button" className= "NextBtn">
-            Next
-            </button>
-        </>
-    )
-}
+  return (
+    <>
+      <button type="button" className="NextBtn">
+        Next
+      </button>
+    </>
+  );
+};
