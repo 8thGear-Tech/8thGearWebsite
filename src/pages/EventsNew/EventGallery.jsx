73ed88ed4b7b92eb79d4639abf459a1dffb2d2e7
adesanyaolu <sanya4reel@gmail.com>
2024-06-28 16:20:41 +0100

    In progress

--- src/pages/EventsNew/EventGallery.jsx
+++ src/pages/EventsNew/EventGallery.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
+import React from 'react';
+import "./eventsNew.css";
 
 const EventGallery = () => {
-  return (
-    <div className='eventsCalendar d-flex flex-column'>
-      <div className="eventsHead d-flex justify-content-center align-items-center">
-            <h3 className="fw-semibold py-4">Gallery</h3>
-        </div>
-        <div className='gallery container'>
-            <div className='row'>
-                <div className='col-12 col-md-6'></div>
-                <div className='col-12 col-md-6'>
-                    <p>
-                    Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-                    Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-                    </p>
-                    <a href="#" className="btn ">Explore</a>
+    return (
+        <div className='eventsCalendar d-flex flex-column'>
+            <div className="eventsHead d-flex justify-content-center align-items-center">
+                <h3 className="fw-semibold py-4">Gallery</h3>
+            </div>
+            <div className='gallery container'>
+                <div className='row'>
+                    <div className='col-12 col-md-6'></div>
+                    <div className='col-12 col-md-6'>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                        </p>
+                        <a href="#" className="btn">Explore</a>
+                    </div>
                 </div>
             </div>
-
         </div>
-    </div>
-  )
+    );
 }
 
-export default EventGallery
+export default EventGallery;
--- src/pages/EventsNew/Hero.jsx
+++ src/pages/EventsNew/Hero.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import "./eventsNew.css"
-
+import React, { useEffect, useState } from 'react';
 import { Navbar29 } from "../../components/Navbar";
+import "./eventsNew.css";
 
 const Hero = () => {
     const [currentTextIndex, setCurrentTextIndex] = useState(0);
@@ -10,34 +9,29 @@ const Hero = () => {
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setFade(false); 
+            setFade(false);
             setTimeout(() => {
                 setCurrentTextIndex((prevIndex) => (prevIndex + 1) % dynamicText.length);
-                setFade(true); 
-            }, 500); 
+                setFade(true);
+            }, 500);
         }, 3000);
 
         return () => clearInterval(interval);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [dynamicText.length]);
 
-  return (
-    <div>
-      <div className="hero text-center position-relative">
+    return (
+        <div className="hero text-center position-relative">
             <Navbar29 />
-            <div className="d-block w-100" style={{
-                    backgroundImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8)),url(/images/eventsNew/eventHero.png)',
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                    height: '100vh',
-                    textAlign: 'left',
-                }}>
-                <h3 className='heroText'>Our events are<span className={`heroTextSpan p-3 ${fade ? 'fade-in' : 'fade-out'}`}> {dynamicText[currentTextIndex]} </span></h3>
-                </div>
-            {/* <img src="/images/eventsNew/eventsHero.png" alt="" /> */}
+            <div className="hero-background">
+                <h3 className='heroText'>
+                    Our events are
+                    <span className={`heroTextSpan p-3 ${fade ? 'fade-in' : 'fade-out'}`}>
+                        {dynamicText[currentTextIndex]}
+                    </span>
+                </h3>
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Hero
+export default Hero;
