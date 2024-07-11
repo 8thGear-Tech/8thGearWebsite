3bf59c20d747a92a471b1f5479fc18dd73fd411f
adesanyaolu <sanya4reel@gmail.com>
2024-07-11 18:57:31 +0100

    Made changes to the coworking page

--- src/pages/coworkingNew/Cards.jsx
+++ src/pages/coworkingNew/Cards.jsx
@@ -1,28 +1,25 @@
 import { useState } from "react";
-import PropTypes from "prop-types";
 
-const Card = ({ imgSrc, text }) => {
+const Card = ({ imgSrc, text, bgImgSrc }) => {
   const [isHovered, setIsHovered] = useState(false);
-
-  Card.propTypes = {
-    imgSrc: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-  };
 
   return (
     <div
       className={`d-flex flex-column align-items-center ${
         isHovered ? "card-bg-img" : "card"
       }`}
+      style={{
+        backgroundImage: isHovered ? `url(${bgImgSrc})` : 'none',
+      }}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <img
         src={imgSrc}
         alt="card"
-        className={`${isHovered ? "invisible" : ""}`}
+        className={`${isHovered ? "invisible" : ""} py-2`}
       />
-      <div className={`cardText ${isHovered ? "invisible" : ""} know-us-card`}>
+      <div className={`cardText ${isHovered ? "invisible" : ""} py-3`}>
         <p className="text-white fs-6 text-center">{text}</p>
       </div>
     </div>
