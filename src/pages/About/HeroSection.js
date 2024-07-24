0f06a72336055909008f77cce158f90bef00e288
8thGear-Tech <opeyemibioku@gmail.com>
2024-07-24 10:51:55 +0100

    updated the about page

--- src/pages/About/HeroSection.js
+++ src/pages/About/HeroSection.js
@@ -9,9 +9,13 @@ const HeroSection = () => {
           Fueling Innovation, Empowering Entrepreneurs and Transforming futures
         </h2> */}
         <h2 className="animated-text fw-bold text-white">
-          {" "}
+          {/* <h2 className="d-none d-lg-block d-md-none d-sm-none d-xs-none animated-text fw-bold text-white"> */}{" "}
           We are your one-stop shop to achieve your Venture Building Dream.
         </h2>
+        {/* <h3 className="d-xs-block d-sm-block d-md-block d-lg-none animated-text fw-bold text-white">
+          {" "}
+          We are your one-stop shop to achieve your Venture Building Dream.
+        </h3> */}
       </div>
       <div className="about-content text-white text-center">
         {/* <p className="fs-5 fw-normal lh-lg">
