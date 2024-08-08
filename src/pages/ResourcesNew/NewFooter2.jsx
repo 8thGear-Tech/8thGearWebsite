6e5b732dbcb61fa32d7c3ecd221be7389b0f6bf7
adesanyaolu <sanya4reel@gmail.com>
2024-08-08 12:36:33 +0100

    New resources page 90% completed

--- src/pages/ResourcesNew/NewFooter2.jsx
+++ src/pages/ResourcesNew/NewFooter2.jsx
@@ -1,7 +1,12 @@
 export default function NewFooter2 () {
     return (
-        <div>
-            Here is a new footer
+        <div className="text-center my-5">
+            <div>
+                <img src="/images/8thgearlogo.png" alt="" width={80} height={80} />
+            </div>
+            <div>
+                <p>Will be updating this with Uche's own, hopefully he has written the footer</p>
+            </div>
         </div>
     )
 }
