9120fd83fca2c7fcff7dcb02b0ffbdd6501dd777
adesanyaolu <sanya4reel@gmail.com>
2024-08-12 18:00:19 +0100

    Footer updated

--- src/components/Hero/LogoScroll.js
+++ src/components/Hero/LogoScroll.js
@@ -1,100 +1,100 @@
-import React from "react";
+// import React from "react";
 
-// sass file
-import "../../sassfiles/pages/projectdelivery/_logoscroll.sass";
+// // sass file
+// import "../../sassfiles/pages/projectdelivery/_logoscroll.sass";
 
-// const logos = [
-//   gizlogo,
-//   itflogo,
-//   lsetflogo,
-//   usadflogo,
-//   s4plogo,
-//   fcmblogo,
-//   Afrilab,
-//   leadAfrica,
-//   Peiges,
-// ];
+// // const logos = [
+// //   gizlogo,
+// //   itflogo,
+// //   lsetflogo,
+// //   usadflogo,
+// //   s4plogo,
+// //   fcmblogo,
+// //   Afrilab,
+//   // leadAfrica,
+// //   Peiges,
+// // ];
 
-// const LogoScroll = () => {
-//     return (
-//         <div className="logo-scroll">
-//             <div className="scroll-container">
-//                 {logos.map((logo, index) => (
-//                     <img key={index} src={logo} alt={`Logo ${index + 1}`} />
-//                 ))}
-//                 {logos.map((logo, index) => (
-//                     <img key={index + logos.length} src={logo} alt={`Logo ${index + 1}`} />
-//                 ))}
+// // const LogoScroll = () => {
+// //     return (
+// //         <div className="logo-scroll">
+// //             <div className="scroll-container">
+// //                 {logos.map((logo, index) => (
+// //                     <img key={index} src={logo} alt={`Logo ${index + 1}`} />
+// //                 ))}
+// //                 {logos.map((logo, index) => (
+// //                     <img key={index + logos.length} src={logo} alt={`Logo ${index + 1}`} />
+// //                 ))}
+// //             </div>
+// //         </div>
+// //     );
+// // };
+
+// const LogoScroll = () => (
+//   <>
+//     <div>
+//       <div className="slider2" style={{ position: "relative", zIndex: 1 }}>
+//         <div className="slide-track2">
+//           {[
+//             gizlogo,
+//             itflogo,
+//             lsetflogo,
+//             usadflogo,
+//             s4plogo,
+//             fcmblogo,
+//             Afrilab,
+//             leadAfrica,
+//             Peiges,
+//           ].map((src, index) => (
+//             <div
+//               className="slide2"
+//               key={index}
+//               style={index === 0 ? { marginLeft: "0px" } : undefined}
+//             >
+//               <img
+//                 src={src}
+//                 alt={`Logo ${index + 1}`}
+//                 className="carousel-logo"
+//                 style={{
+//                   width: "130px",
+//                   height: "100px",
+//                   margin: "0 30px",
+//                 }}
+//               />
 //             </div>
+//           ))}
+//           {[
+//             gizlogo,
+//             itflogo,
+//             lsetflogo,
+//             usadflogo,
+//             s4plogo,
+//             fcmblogo,
+//             Afrilab,
+//             leadAfrica,
+//             Peiges,
+//           ].map((src, index) => (
+//             <div
+//               className="slide2"
+//               key={index}
+//               style={index === 0 ? { marginLeft: "0px" } : undefined}
+//             >
+//               <img
+//                 src={src}
+//                 alt={`Logo ${index + 1}`}
+//                 className="carousel-logo"
+//                 style={{
+//                   width: "130px",
+//                   height: "100px",
+//                   margin: "0 30px",
+//                 }}
+//               />
+//             </div>
+//           ))}
 //         </div>
-//     );
-// };
+//       </div>
+//     </div>
+//   </>
+// );
 
-const LogoScroll = () => (
-  <>
-    <div>
-      <div className="slider2" style={{ position: "relative", zIndex: 1 }}>
-        <div className="slide-track2">
-          {[
-            gizlogo,
-            itflogo,
-            lsetflogo,
-            usadflogo,
-            s4plogo,
-            fcmblogo,
-            Afrilab,
-            leadAfrica,
-            Peiges,
-          ].map((src, index) => (
-            <div
-              className="slide2"
-              key={index}
-              style={index === 0 ? { marginLeft: "0px" } : undefined}
-            >
-              <img
-                src={src}
-                alt={`Logo ${index + 1}`}
-                className="carousel-logo"
-                style={{
-                  width: "130px",
-                  height: "100px",
-                  margin: "0 30px",
-                }}
-              />
-            </div>
-          ))}
-          {[
-            gizlogo,
-            itflogo,
-            lsetflogo,
-            usadflogo,
-            s4plogo,
-            fcmblogo,
-            Afrilab,
-            leadAfrica,
-            Peiges,
-          ].map((src, index) => (
-            <div
-              className="slide2"
-              key={index}
-              style={index === 0 ? { marginLeft: "0px" } : undefined}
-            >
-              <img
-                src={src}
-                alt={`Logo ${index + 1}`}
-                className="carousel-logo"
-                style={{
-                  width: "130px",
-                  height: "100px",
-                  margin: "0 30px",
-                }}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  </>
-);
-
-export default LogoScroll;
+// export default LogoScroll;
--- src/pages/ResourcesNew/NewFooter2.jsx
+++ src/pages/ResourcesNew/NewFooter2.jsx
@@ -1,11 +1,10 @@
+import { FooterNew } from "../../components/newFooter"
+
 export default function NewFooter2 () {
     return (
         <div className="text-center my-5">
             <div>
-                <img src="/images/8thgearlogo.png" alt="" width={80} height={80} />
-            </div>
-            <div>
-                <p>Will be updating this with Uche's own, hopefully he has written the footer</p>
+                <FooterNew />
             </div>
         </div>
     )
