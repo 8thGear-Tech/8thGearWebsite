4bdfa9ddfa8fb4b4f808fd97728f90c22e357d59
adesanyaolu <sanya4reel@gmail.com>
2024-09-04 22:36:40 +0100

    Started the new venture studio screen

---
+++ src/pages/ventureStudio/ventureStudio.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from './Hero'
+
+
+const VentureStudio = () => {
+    return (
+        <div className='venture-studio'>
+           <Hero />
+        </div>
+    )
+}
+
+export default VentureStudio
