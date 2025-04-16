952a7c35d6dee4b3c4be97ceda59cab916aa971d
opeyemibioku <opeyemibioku@gmail.com>
2025-04-16 14:23:01 +0100

    updated the venture-studio

--- src/pages/ventureStudio/ventureStudio.jsx
+++ src/pages/ventureStudio/ventureStudio.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Hero from "./Hero";
 import CoreServices from "./coreServices";
 import HowWeDoIt from "./howWeDoIt";
 import ImpactMetrics from "./impactMetrics";
 import PortfolioCompanies from "./PortfolioCompanies";
 import { ApplyNowBtn } from "./Button";
+import { Document, Page, pdfjs } from 'react-pdf';
+import { EmbedPDF } from '@simplepdf/react-embed-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+
+
+
+// Required setup for PDF.js worker
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js`;
 
 const VentureStudio = () => {
   return (
@@ -12,36 +20,41 @@ const VentureStudio = () => {
       <div>
         <Hero />
       </div>
-      <div className="container my-5">
-       {/* <iframe
-  src="/pdfs/IRE_Curriculum&Content.pdf"
-  width="100%"
-  height="800px"
-  style={{
-    border: 'none',
-    outline: 'none',
-    margin: 0,
-    padding: 0,
-  }}
-  title="Embedded PDF"
-/> */}
-
-<embed
-  src="/pdfs/IRE_Curriculum&Content.pdf"
-  type="application/pdf"
-  width="100%"
-  height="800"
-  style={{ border: 'none', margin: 0, padding: 0 }}
-/>
-
-
-        {/* <iframe
-          src="/pdfs/IRE_Curriculum&Content.pdf#toolbar=0&navpanes=0&scrollbar=0"
-          width="100%"
-          height="800"
-          style={{ border: "none" }}
-          title="Inline PDF"
+      <div className="w-full px-0 my-5">
+      {/* <iframe
+          src="/pdfs/IRE_Curriculum&Content.pdf#toolbar=0&navpanes=0&scrollbar=0" 
+          className="w-full lg:w-4/5 mx-auto h-screen"
+          style={{ minHeight: "800px" }}
+          title="PDF Document"
+        ></iframe> */}
+         <div className="relative w-full center">
+        <div className="absolute left-0 right-0" style={{ width: "100vw", marginLeft: "calc(-50vw + 50%)" }}>
+          <object
+            data="/pdfs/IRE_Curriculum&Content.pdf#toolbar=0"
+            type="application/pdf"
+            className="w-full"
+            style={{ height: "90vh", minHeight: "700px" }}
+          >
+            <p>Your browser doesn't support embedded PDFs.</p>
+          </object>
+        </div>
+      </div>
+         {/* <object
+          data="/pdfs/IRE_Curriculum&Content.pdf"
+          type="application/pdf"
+          className="w-full md:w-4/5 mx-auto"
+          style={{ height: "90vh", minHeight: "600px" }}
+        ></object> */}
+         {/* <embed 
+          src="/pdfs/IRE_Curriculum&Content.pdf" 
+          type="application/pdf"
+          className="w-full h-screen" 
+          style={{ minHeight: "800px" }}
         /> */}
+      {/* <Document file="/pdfs/IRE_Curriculum&Content.pdf">
+          <Page pageNumber={1} />
+        </Document> */}
+        {/* <PDFSection/> */}
       </div>
       <div className="text-center">
         <ApplyNowBtn buttonText={"Apply Now"} />
@@ -76,5 +89,23 @@ const VentureStudio = () => {
     </div>
   );
 };
+
+const PDFSection = () => {
+  const [numPages, setNumPages] = useState(null);
+
+  return (
+    <div className="flex flex-col items-center my-10">
+      <Document
+        file="/pdfs/IRE_Curriculum&Content.pdf"
+        onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+      >
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page key={`page_${index + 1}`} pageNumber={index + 1} width={900} />
+        ))}
+      </Document>
+    </div>
+  );
+};
+
 
 export default VentureStudio;
